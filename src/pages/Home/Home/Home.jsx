ae77d0a944544fa2ccc70cc95b2abfed077e2eec
Masudur Rabbi Rahat <rabbirahat9@gmail.com>
2025-03-20 22:24:58 +0600

    swiper JS to create a category Swiper

--- src/pages/Home/Home/Home.jsx
+++ src/pages/Home/Home/Home.jsx
@@ -1,10 +1,12 @@
 import Banner from "../Banner/Banner";
+import Category from "../Category/Category";
 
 
 const Home = (props) => {
     return (
         <div>
             <Banner></Banner>
+            <Category></Category>
         </div>
     );
 };
