6dd84342b0a3ce000ca2e5ee04573d08fbfc570f
Masudur Rabbi Rahat <rabbirahat9@gmail.com>
2025-03-21 16:42:51 +0600

    Show Popular menu items in the home page
    new files:
    Home.jsx
    PopularMenu.jsx
    MenuItem.jsx

--- src/pages/Home/Home/Home.jsx
+++ src/pages/Home/Home/Home.jsx
@@ -1,12 +1,14 @@
 import Banner from "../Banner/Banner";
 import Category from "../Category/Category";
+import PopularMenu from "../PopularMenu/PopularMenu";
 
 
 const Home = (props) => {
     return (
         <div>
             <Banner></Banner>
-            <Category></Category>   
+            <Category></Category>
+            <PopularMenu></PopularMenu>  
         </div>
     );
 };
